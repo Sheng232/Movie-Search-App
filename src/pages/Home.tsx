cd087fdb40e396fe028f69698d4fa488e69e4146
sheng232 <lowellchessofficial@gmail.com>
2024-12-28 23:03:46 -0800

    style: update styles for improved UI and add WatchList component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,10 +28,12 @@ function App() {
   }
 
 
+  //The movie data is stored in the local storage - to be displayed
   const [movieData, setMovieData] = useState<Movies>(
     JSON.parse(localStorage.getItem("movies") || "[]")
   );
 
+  //Fetch trending movies
   useEffect(()=>{
     const fetchTrendingMovies = async() =>{
         try{
@@ -47,16 +49,18 @@ function App() {
         }
     }
     fetchTrendingMovies();
-}, [])
+}, [movieData]);
 
+  //The query state is updated when the user types in the search bar
   const [movieQuery, setMovieQuery] = useState<string>("");
 
+  //Fetch movie data based on the query
   function fetchMovie(event: { preventDefault: () => void }) {
     event.preventDefault();
     const fetchData = async (query: string) => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/search/movie?api_key=7c6359fb38405964278bde77066e6096&language=en-US&query=${query}&page=1&include_adult=false`
+          `https://api.themoviedb.org/3/search/movie?api_key=7c6359fb38405964278bde77066e6096&language=en-US&query=${query}`
         );
         const data = await res.json();
         setMovieData(data.results);
@@ -69,30 +73,33 @@ function App() {
   }
 
   
+  //Sync the movie data with the local storage
   useEffect(()=>{
     const data = JSON.stringify(movieData);
     JSON.stringify(localStorage.setItem("movies", data));
   }, [movieData])
 
+  //Update the query state based on the user input
   function updateQueryState(event: React.ChangeEvent<HTMLInputElement>) {
     setMovieQuery(event.target.value);
   }
 
+  //Display the movie data
   const displayMovies = movieData.map((movie: Movie, index: number) => {
     const { id, title, poster_path, release_date, name, first_air_date} = movie;
     return (
       poster_path && (
-        <div key={index} className={appStyle.movieContainer}>
-          <LikeButton />
-          <Link to={`${id}`}>
-            <h2>{title ? title : name}</h2>
-            <p>{release_date ? release_date : first_air_date}</p>
-            <img
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${poster_path}`}
-              alt=""
-            />
-          </Link>
-        </div>
+          <div key={index} className={appStyle.movieContainer}>
+              <LikeButton />
+              <Link to={`${id}`}>
+                <h2>{title ? title : name}</h2>
+                <p>{release_date ? release_date : first_air_date}</p>
+                <img
+                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${poster_path}`}
+                  alt=""
+                />
+              </Link>
+          </div>
       )
     );
   });
@@ -108,6 +115,11 @@ function App() {
           value={movieQuery}
         />
         <button type="submit">Search</button>
+        <button onClick={()=>{
+          setMovieData([]);
+        }}
+        >Trending</button>
+
       </form>
 
       <section className={appStyle.movieData}>
